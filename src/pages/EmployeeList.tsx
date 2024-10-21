a214eb73636925d91f0fb22d7d176456164b2e8c
Claire Royer <claireroyer57@gmail.com>
2024-10-21 19:01:05 +0200

    Continue order for table

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -50,26 +50,35 @@ const EmployeeList = (): JSX.Element => {
   ];
 
   const [orderedList, setOrderedList] = useState<IData[]>([...fakeData]);
+  const [isFirstNameOrdered, setIsFirstNameOrdered] = useState(false);
+  const [isLastNameOrdered, setIsLastNameOrdered] = useState(false);
+  const [isDateOrdered, setIsDateOrdered] = useState(false);
+  const [isDepartmentOrdered, setIsDepartmentOrdered] = useState(false);
 
   const handlerOrder = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>): void => {
     const event = e.target as HTMLTableCellElement;
     const data = fakeData.sort((a, b): number => {
       if (event.id === "firstName") {
-        if (a.firstName < b.firstName) {
-          return -1;
-        } else if (a.firstName > b.firstName) {
-          return 1;
-        }
+        setIsFirstNameOrdered(prev => !prev);
+        setIsLastNameOrdered(false);
+        setIsDateOrdered(false);
+        setIsDepartmentOrdered(false);
+        return isFirstNameOrdered
+          ? a.firstName > b.firstName ? -1 : 1
+          : a.firstName < b.firstName ? -1 : 1;
       } else if (event.id === "lastName") {
-        if (a.lastName < b.lastName) {
-          return -1;
-        } else if (a.lastName > b.lastName) {
-          return 1;
-        }
-      }
-
-      if (event.id === "startDate") {
+        // Work in progress
+        setIsLastNameOrdered(prev => !prev);
+        setIsFirstNameOrdered(false);
+        setIsDateOrdered(false);
+        setIsDepartmentOrdered(false);
+        return isLastNameOrdered
+          ? a.lastName > b.lastName ? -1 : 1
+          : a.lastName < b.lastName ? -1 : 1;
+        // ------------------------------------
+      } else if (event.id === "startDate") {
         let dateA: Date, dateB: Date;
+        setIsDateOrdered(prev => !prev);
 
         if (typeof a.startDate === "string") {
           dateA = new Date(a.startDate.split("-").reverse().join("-"));
@@ -82,8 +91,17 @@ const EmployeeList = (): JSX.Element => {
         } else {
           dateB = b.startDate;
         }
-
-        return dateA.getTime() - dateB.getTime();
+        return isDateOrdered
+          ? dateA.getTime() > dateB.getTime() ? -1 : 1
+          : dateA.getTime() < dateB.getTime() ? -1 : 1;
+      } else if (event.id === "departments") {
+        setIsDepartmentOrdered(prev => !prev);
+        setIsFirstNameOrdered(false);
+        setIsLastNameOrdered(false);
+        setIsDateOrdered(false);
+        return isDepartmentOrdered
+          ? a.departments > b.departments ? -1 : 1
+          : a.departments < b.departments ? -1 : 1;
       }
       return 0;
     });
@@ -116,10 +134,14 @@ const EmployeeList = (): JSX.Element => {
         <caption className="hidden">Employee list</caption>
         <thead className="text-lg">
         <tr className="cursor-pointer bg-sky-950">
-          <th scope="col" className="rounded border-t border-none px-1 py-2" id="firstName" onClick={handlerOrder}>First
-            Name
+          <th scope="col" className="rounded border-t border-none px-1 py-2" id="firstName" onClick={handlerOrder}>
+            First Name
           </th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="lastName">Last Name</th>
+          <th scope="col" className="px-1 py-2" onClick={(e) => {
+            handlerOrder(e);
+            // setIsLastNameOrdered(prev => !prev);
+          }} id="lastName">Last Name
+          </th>
           <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="startDate">Start Date</th>
           <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="departments">Department</th>
           <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="birthDate">Date of Birth</th>
