de277e8538beb280e7fe20fe2e14240d293bc435
Claire Royer <claireroyer57@gmail.com>
2024-10-22 18:20:18 +0200

    Add sorted function to all table cells head

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -48,64 +48,67 @@ const EmployeeList = (): JSX.Element => {
       zipCode: "77002",
     },
   ];
-
+  //
+  // ------------------------------------------Add id to interface-----------------------------------------------------
+  //
   const [orderedList, setOrderedList] = useState<IData[]>([...fakeData]);
   const [isFirstNameOrdered, setIsFirstNameOrdered] = useState(false);
   const [isLastNameOrdered, setIsLastNameOrdered] = useState(false);
-  const [isDateOrdered, setIsDateOrdered] = useState(false);
+  const [isStartDateOrdered, setIsStartDateOrdered] = useState(false);
   const [isDepartmentOrdered, setIsDepartmentOrdered] = useState(false);
+  const [isBirtDateOrdered, setIsBirtDateOrdered] = useState(false);
+  const [isStreetOrdered, setIsStreetOrdered] = useState(false);
+  const [isCityOrdered, setIsCityOrdered] = useState(false);
+  const [isStateOrdered, setIsStateOrdered] = useState(false);
+  const [isZipCodeOrdered, setIsZipCodeOrdered] = useState(false);
 
-  const handlerOrder = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>): void => {
-    const event = e.target as HTMLTableCellElement;
-    const data = fakeData.sort((a, b): number => {
-      if (event.id === "firstName") {
-        setIsFirstNameOrdered(prev => !prev);
-        setIsLastNameOrdered(false);
-        setIsDateOrdered(false);
-        setIsDepartmentOrdered(false);
-        return isFirstNameOrdered
-          ? a.firstName > b.firstName ? -1 : 1
-          : a.firstName < b.firstName ? -1 : 1;
-      } else if (event.id === "lastName") {
-        // Work in progress
-        setIsLastNameOrdered(prev => !prev);
-        setIsFirstNameOrdered(false);
-        setIsDateOrdered(false);
-        setIsDepartmentOrdered(false);
-        return isLastNameOrdered
-          ? a.lastName > b.lastName ? -1 : 1
-          : a.lastName < b.lastName ? -1 : 1;
-        // ------------------------------------
-      } else if (event.id === "startDate") {
-        let dateA: Date, dateB: Date;
-        setIsDateOrdered(prev => !prev);
+  const handleOrder = (
+    setCurrentState: React.Dispatch<React.SetStateAction<boolean>>, currentState: boolean, property: keyof IData,
+    resetState1: React.Dispatch<React.SetStateAction<boolean>>, resetState2: React.Dispatch<React.SetStateAction<boolean>>,
+    resetState3: React.Dispatch<React.SetStateAction<boolean>>, resetState4: React.Dispatch<React.SetStateAction<boolean>>,
+    resetState5: React.Dispatch<React.SetStateAction<boolean>>, resetState6: React.Dispatch<React.SetStateAction<boolean>>,
+    resetState7: React.Dispatch<React.SetStateAction<boolean>>, resetState8: React.Dispatch<React.SetStateAction<boolean>>,
+    e: React.MouseEvent<HTMLTableCellElement, MouseEvent>): void => {
+    const event = e.currentTarget;
 
-        if (typeof a.startDate === "string") {
-          dateA = new Date(a.startDate.split("-").reverse().join("-"));
-        } else {
-          dateA = a.startDate;
+    const data = orderedList.sort((a, b) => {
+      if (event.id === "startDate" || event.id === "birtDate") {
+        let dateA = a[property] as string | Date;
+        let dateB = b[property] as string | Date;
+
+        if (typeof dateA === "string") {
+          dateA = new Date(dateA.split("-").reverse().join("-"));
         }
 
-        if (typeof b.startDate === "string") {
-          dateB = new Date(b.startDate.split("-").reverse().join("-"));
-        } else {
-          dateB = b.startDate;
+        if (typeof dateB === "string") {
+          dateB = new Date(dateB.split("-").reverse().join("-"));
         }
-        return isDateOrdered
+
+        return currentState
           ? dateA.getTime() > dateB.getTime() ? -1 : 1
           : dateA.getTime() < dateB.getTime() ? -1 : 1;
-      } else if (event.id === "departments") {
-        setIsDepartmentOrdered(prev => !prev);
-        setIsFirstNameOrdered(false);
-        setIsLastNameOrdered(false);
-        setIsDateOrdered(false);
-        return isDepartmentOrdered
-          ? a.departments > b.departments ? -1 : 1
-          : a.departments < b.departments ? -1 : 1;
+      } else {
+        const dataA = a[property];
+        const dataB = b[property];
+
+        if (dataA > dataB) {
+          return currentState ? -1 : 1;
+        } else if (dataA < dataB) {
+          return currentState ? 1 : -1;
+        }
       }
       return 0;
     });
 
+    setCurrentState(prev => !prev);
+    resetState1(false);
+    resetState2(false);
+    resetState3(false);
+    resetState4(false);
+    resetState5(false);
+    resetState6(false);
+    resetState7(false);
+    resetState8(false);
     setOrderedList(data);
   };
 
@@ -134,22 +137,51 @@ const EmployeeList = (): JSX.Element => {
         <caption className="hidden">Employee list</caption>
         <thead className="text-lg">
         <tr className="cursor-pointer bg-sky-950">
-          <th scope="col" className="rounded border-t border-none px-1 py-2" id="firstName" onClick={handlerOrder}>
+          <th scope="col" className="rounded-tl border-none px-1 py-2" id="firstName"
+              onClick={(event) => handleOrder(setIsFirstNameOrdered, isFirstNameOrdered, "firstName", setIsLastNameOrdered,
+                setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>
             First Name
           </th>
-          <th scope="col" className="px-1 py-2" onClick={(e) => {
-            handlerOrder(e);
-            // setIsLastNameOrdered(prev => !prev);
-          }} id="lastName">Last Name
+          <th scope="col" className="px-1 py-2" id="lastName"
+              onClick={(event) => handleOrder(setIsLastNameOrdered, isLastNameOrdered, "lastName", setIsFirstNameOrdered,
+                setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>Last Name
           </th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="startDate">Start Date</th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="departments">Department</th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="birthDate">Date of Birth</th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="street">Street</th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="city">City</th>
-          <th scope="col" className="px-1 py-2" onClick={handlerOrder} id="state">State</th>
-          <th scope="col" className="rounded border-t border-none px-1 py-2" onClick={handlerOrder} id="zipCode">Zip
-            Code
+          <th scope="col" className="px-1 py-2" id="startDate"
+              onClick={(event) => handleOrder(setIsStartDateOrdered, isStartDateOrdered, "startDate", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>Start Date
+          </th>
+          <th scope="col" className="px-1 py-2" id="departments"
+              onClick={(event) => handleOrder(setIsDepartmentOrdered, isDepartmentOrdered, "departments", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsBirtDateOrdered, setIsStreetOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>Department
+          </th>
+          <th scope="col" className="px-1 py-2" id="birthDate"
+              onClick={(event) => handleOrder(setIsBirtDateOrdered, isBirtDateOrdered, "birthDate", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsDepartmentOrdered, setIsStreetOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>Date of Birth
+          </th>
+          <th scope="col" className="px-1 py-2" id="street"
+              onClick={(event) => handleOrder(setIsStreetOrdered, isStreetOrdered, "street", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsCityOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>Street
+          </th>
+          <th scope="col" className="px-1 py-2" id="city"
+              onClick={(event) => handleOrder(setIsCityOrdered, isCityOrdered, "city", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered,
+                setIsStateOrdered, setIsZipCodeOrdered, event)}>City
+          </th>
+          <th scope="col" className="px-1 py-2" id="state"
+              onClick={(event) => handleOrder(setIsStateOrdered, isStateOrdered, "state", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered,
+                setIsCityOrdered, setIsZipCodeOrdered, event)}>State
+          </th>
+          <th scope="col" className="rounded border-none px-1 py-2" id="zipCode"
+              onClick={(event) => handleOrder(setIsZipCodeOrdered, isZipCodeOrdered, "zipCode", setIsFirstNameOrdered,
+                setIsLastNameOrdered, setIsStartDateOrdered, setIsDepartmentOrdered, setIsBirtDateOrdered, setIsStreetOrdered,
+                setIsCityOrdered, setIsStateOrdered, event)}>Zip Code
           </th>
         </tr>
         </thead>
