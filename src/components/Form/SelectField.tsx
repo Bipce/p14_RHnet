5f0a952bbbdbced9862233c9d671ad4049cee381
Claire Royer <claireroyer57@gmail.com>
2024-11-25 10:45:05 +0100

    Add className to select options in form

--- src/components/Form/SelectField.tsx
+++ src/components/Form/SelectField.tsx
@@ -12,7 +12,7 @@ const SelectField: React.FC<ISelectData> = ({ id, register, htmlFor, label, data
     <FieldWrapper htmlFor={htmlFor} label={label}>
       <select id={id} {...register}
               className="rounded border-2 border-slate-600 bg-slate-900 px-1 text-slate-100 outline-none focus:border-sky-400">
-        {data.map((item, i) => <option key={i}>{item}</option>)}
+        {data.map((item, i) => <option key={i} className="bg-slate-900 text-slate-100">{item}</option>)}
       </select>
     </FieldWrapper>
   );
